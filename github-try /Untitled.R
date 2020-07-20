a2064e756cc602e02e66f1f16b2f3f14bab72a17
Munshi Md Rasel <munshimdrasel@gwu.edu>
2020-07-20 13:53:21 -0400

    whats wrong?

---
+++ github-try /Untitled.R
@@ -0,0 +1,53 @@
+# to remove everything from global environment
+rm(list = ls())
+
+getwd()
+
+setwd("/Users/munshirasel/Google Drive/R/github-try/github-try")
+
+a <- 2a <- 2
+b <- -3
+sig_sq <- 0.5
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+#> [1] 0.554
+write(avg_x, "avg_x.txt")
+plot(x, y)
+abline(a, b, col = "purple")
+dev.print(pdf, "toy_line_plot.pdf")
+a <- 2
+b <- -3
+sig_sq <- 0.5
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+#> [1] 0.554
+write(avg_x, "avg_x.txt")
+plot(x, y)
+abline(a, b, col = "purple")
+dev.print(pdf, "toy_line_plot.pdf")
+
+b <- -3
+sig_sq <- 0.5
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+#> [1] 0.554
+write(avg_x, "avg_x.txt")
+plot(x, y)
+abline(a, b, col = "purple")
+dev.print(pdf, "toy_line_plot.pdf")
+a <- 2
+b <- -3
+sig_sq <- 0.5
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+#> [1] 0.554
+write(avg_x, "avg_x.txt")
+plot(x, y)
+abline(a, b, col = "purple")
+dev.print(pdf, "toy_line_plot.pdf")
+
+#install.packages("tidyverse")
---
+++ github-try /happy-git.R
@@ -0,0 +1,72 @@
+# to remove everything from global environment
+rm(list = ls())
+
+getwd()
+
+setwd("/Users/munshirasel/Google Drive/R/github-try/github-try")
+
+a <- 2a <- 2
+b <- -3
+sig_sq <- 0.5
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+#> [1] 0.554
+write(avg_x, "avg_x.txt")
+plot(x, y)
+abline(a, b, col = "purple")
+dev.print(pdf, "toy_line_plot.pdf")
+a <- 2
+b <- -3
+sig_sq <- 0.5
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+#> [1] 0.554
+write(avg_x, "avg_x.txt")
+plot(x, y)
+abline(a, b, col = "purple")
+dev.print(pdf, "toy_line_plot.pdf")
+
+b <- -3
+sig_sq <- 0.5
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+#> [1] 0.554
+write(avg_x, "avg_x.txt")
+plot(x, y)
+abline(a, b, col = "purple")
+dev.print(pdf, "toy_line_plot.pdf")
+a <- 2
+b <- -3
+sig_sq <- 0.5
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+#> [1] 0.554
+write(avg_x, "avg_x.txt")
+plot(x, y)
+abline(a, b, col = "purple")
+dev.print(pdf, "toy_line_plot.pdf")
+
+
+#control+ shift + c for decommenting code and commenting code
+
+# install.packages("tidyverse")
+
+
+
+
+library()
+
+#to see R version
+R.version.string
+
+#This is a great time to upgrade to the latest Preview version. Download it here:
+# https://rstudio.com/products/rstudio/download/preview/
+
+update.packages(ask = FALSE, checkBuilt = TRUE)
+
+getwd()
+setwd("/Users/munshirasel/github-try/github-try")
